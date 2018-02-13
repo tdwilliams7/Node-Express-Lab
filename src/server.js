64c45cd49a9b4cc8c68c65892fce9656bb7c135a
Troy Williams <troyDonwilliams@gmail.com>
2018-02-12 20:02:59 -0600

    get works with a term and without

--- src/server.js
+++ src/server.js
@@ -1,16 +1,43 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
 const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [
+  {
+    title: "The post title",
+    contents: "The post contents"
+  }
+];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+server.get("/posts", (req, res) => {
+  const { term } = req.query;
+  if (term) {
+    const newPosts = posts.filter(post => {
+      const tempTitle = post.title.split(" ");
+      const tempContents = post.contents.split(" ");
+      console.log(tempContents);
+      if (tempTitle.includes(term) || tempContents.includes(term)) {
+        return post;
+      }
+    });
+    if (newPosts.length) {
+      res.send(newPosts);
+    } else {
+      res.send("No posts match");
+    }
+  }
+  res.send(posts);
+});
+
+// server.get("/post/:term?", (req, res) => {});
 
 module.exports = { posts, server };
